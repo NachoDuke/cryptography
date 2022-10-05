4842ad04618ec7312759f1914d35235634d21891
NachoDuke <premankur.qb9@gmail.com>
2022-10-05 22:15:50 +0530

    Added a menu to choose which encryption to use

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from src.DiffieHellman import *
 from src.RSA import *
 
+def encrypt():
+    choice=int(input("Enter 0 to use Diffie Hellman and 1 to use RSA encryption"))
+    if(choice==0):
+        DHKey()
+    elif(choice==1):
+        RSAkey()
+    else:
+        print("Input a valid choice")
+        encrypt()
+
 if __name__ =="__main__":
-    #DHKey()
-    RSAkey()
+    encrypt()
